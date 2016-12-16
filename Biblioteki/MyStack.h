1b2a40ee1665757f8f508963b8b74c06bd76276e
Przemek Kołodziejczyk <kemeros13@wp.pl>
2016-12-16 10:50:06 +0100

    Kompilator z mnozeniem i dzieleniem

---
+++ Biblioteki/MyStack.h
@@ -0,0 +1,27 @@
+#ifndef MYSTACK_H
+#define MYSTACK_H
+
+#include <stdlib.h>
+#include <string>
+using namespace std;
+
+class MyStack
+{
+    private:
+        /*element w stosie, zawiera wartosc i swojego poprzednika*/
+        struct element{
+            element *prev; //poprzednik elementu
+            string value; //wartosc w elemencie
+        };
+        element *top = NULL; //wskaznik na top'a, czyli element ktory zdejmiemy
+    public:
+        /*Funkcja dodajaca element na stos*/
+        void push(string new_value); //nowym topem jest wlasnie dodany elelemnt
+        /*Funkcja zdejmujaca elelemnt ze stosu*/
+        string pop();
+        /*Funkcja mowiaca czy stos jest pust*/
+        bool jest_pusty();
+
+};
+
+#endif // MYSTACK_H
